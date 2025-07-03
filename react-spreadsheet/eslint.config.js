851c8e66995b14ad4798b414f5f540d5d2d1d01d
Syed-Abdul-Aleem-Hussaini <aleemsd127@gmail.com>
2025-07-04 01:46:09 +0530

    deployment error solving

--- react-spreadsheet/eslint.config.js
+++ react-spreadsheet/eslint.config.js
@@ -4,6 +4,11 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import prettier from 'eslint-config-prettier'
+
+
+
+
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -14,6 +19,7 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettier
     ],
     languageOptions: {
       ecmaVersion: 2020,
